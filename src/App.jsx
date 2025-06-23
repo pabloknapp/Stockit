6f142dea1c7476ee6214138d877122e0f31c93df
Pablo Knapp <pabloknappdc@gmail.com>
2025-06-23 08:22:42 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+        <h1 class="text-3xl font-bold text-blue-900 underline">teste</h1>
+        <h1 class="text-3xl font-bold text-blue-900">stockit</h1>
+        <img src="src\Assets\logo-stockit.png" alt="" />
+    </>
+  )
+}
+
+export default App

6b489eb4226c9ee77fb53a26bd346cb68d06a258
PABLO KNAPP DA CRUZ <03271281009@senacrs.edu.br>
2025-06-23 11:55:46 -0300

    Adicionado Menu lateral, Diretórios: Styles, Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
+import './Styles/Variables.css'
+import Menu from './Components/Menu'
 
 function App() {
 
 
   return (
     <>
+        <Menu />
         <h1 class="text-3xl font-bold text-blue-900 underline">teste</h1>
         <h1 class="text-3xl font-bold text-blue-900">stockit</h1>
-        <img src="src\Assets\logo-stockit.png" alt="" />
     </>
   )
 }
---
+++ src/Components/Menu.jsx
@@ -0,0 +1,22 @@
+import Logo from '../Assets/logo-stockit.png'
+import '../Styles/Variables.css'
+
+function Menu() {
+
+  return (
+    <> 
+        <header className='flex flex-col items-center bg-cinza-escuro text-white w-[25%] h-lvw'>
+            <img className='mb-[30%] mt-[10%]' src={Logo} alt="" />
+            <ul className='flex flex-col gap-5'>
+                <li>Visão geral</li>
+                <li>Produtos</li>
+                <li>Gerenciar produtos</li>
+                <li>Relatório</li>
+                <li>Histórico</li>
+            </ul>
+        </header>
+    </>
+  )
+}
+
+export default Menu

7594815ece132aa1160d17159dd89f4ba140bf92
luis <matheusdsf94@gmail.com>
2025-06-25 01:07:15 -0300

    adicionado icones e uma parte do menu do mobile

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,7 @@ function App() {
   return (
     <>
         <Menu />
-        <h1 class="text-3xl font-bold text-blue-900 underline">teste</h1>
-        <h1 class="text-3xl font-bold text-blue-900">stockit</h1>
+        
     </>
   )
 }
